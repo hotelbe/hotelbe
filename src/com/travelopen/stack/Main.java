e2862a9e8432eb68a8b4fe3aa32519ac5505c45e
shiwei.gui@gmail.com <GnH@0829>
2018-11-16 20:56:01 +0800

    commited

--- src/com/travelopen/stack/Main.java
+++ src/com/travelopen/stack/Main.java
@@ -11,6 +11,6 @@ public class Main {
 
     public static void main(String[] args) {
         byte[] space1 = new byte[_1MB*4];
-        byte[] space2 = new byte[_1MB*4];
+        byte[] space2 = new byte[_1MB*8];
     }
 }
