8b3ee8ed2f58b757995adb098f5f9870cc9f91a7
shiwei.gui@gmail.com <GnH@0829>
2018-11-29 14:47:26 +0800

    commited 20181116-3

--- src/com/travelopen/stack/Wuhan.java
+++ src/com/travelopen/stack/Wuhan.java
@@ -1,5 +1,9 @@
 package com.travelopen.stack;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @project: helloworld
  * @auther: shiwei
@@ -7,4 +11,46 @@ package com.travelopen.stack;
  * @description: TODO
  */
 public class Wuhan {
+    public static void main(String[] args) {
+        List<User> users = new ArrayList<User>();
+        users.add(new User(20,"张三"));
+        users.add(new User(22,"李四"));
+        users.add(new User(21,"王五"));
+        users.add(new User(24,"钱六"));
+
+        users.forEach((User user)-> System.out.println(user.toString()));
+
+    }
+
+    private static class User {
+        int age;
+        String name;
+
+        public User(int age, String name) {
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        @Override
+        public String toString() {
+            return "User{" +
+                    "age=" + age +
+                    ", name='" + name + '\'' +
+                    '}';
+        }
+    }
 }
