ef0e59b685ff2412156ba363104d9ccf39f7a667
shiwei.gui@gmail.com <GnH@0829>
2018-12-21 11:32:50 +0800

    commited 20181221

--- src/com/travelopen/stack/Main.java
+++ src/com/travelopen/stack/Main.java
@@ -1,5 +1,10 @@
 package com.travelopen.stack;
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @project: helloworld
  * @auther: shiwei
@@ -14,5 +19,19 @@ public class Main {
         byte[] space2 = new byte[_1MB*8];
         byte[] space3 = new byte[_1MB*4];
         byte[] space4 = new byte[_1MB*4];
+
+
+        List<User> users = new ArrayList<>();
+        users.add(new User(20, "张三"));
+        users.add(new User(22, "李四"));
+        users.add(new User(21, "王五"));
+        users.add(new User(24, "钱六"));
+
+        users.forEach((User user) -> System.out.println(user.toString()));
+
+        // for (int i = 0; i < users.size(); i++) {
+        //     User user = users.get(i);
+        //     System.out.println(user.toString());
+        // }
     }
 }
--- src/com/travelopen/stack/TestMain3.java
+++ src/com/travelopen/stack/TestMain3.java
@@ -6,7 +6,7 @@ package com.travelopen.stack;
  * @date: 2018/11/16 13:49
  * @description: TODO
  */
-public class TeatMain3 {
+public class TestMain3 {
     public static void main(String[] args) {
         long start =System.currentTimeMillis();
         for (int i = 0; i < 5000000; i++) {
